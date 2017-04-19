37d43110364b206457f8d852dd7d21a399fee45a
JanHuang <bboyjanhuang@gmail.com>
2017-04-19 20:04:58 +0800

    upgrade to 3.1

--- config/app.php
+++ config/app.php
@@ -11,29 +11,36 @@ return [
     /**
      * The application name.
      */
-    'name' => 'commentd',
+    'name' => 'comment-d',
 
-    /**
-     * Application environment local/dev/prod
+    /*
+     * Exception handle
      */
-    'environment' => 'prod',
-
-    /**
-     * Application timezone
-     */
-    'timezone' => 'PRC',
+    'exception' => [
+        'handle' => function (Exception $e) {
+            return [
+                'msg' => $e->getMessage(),
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'trace' => explode("\n", $e->getTraceAsString()),
+            ];
+        },
+    ],
 
     /**
      * Application logger path
      */
     'log' => [
-        \Monolog\Handler\StreamHandler::class, // 错误日志
+        [\Monolog\Handler\StreamHandler::class, 'error.log', \Monolog\Logger::ERROR],
     ],
 
     /**
      * Bootstrap service.
      */
     'services' => [
+        \FastD\ServiceProvider\RouteServiceProvider::class,
+        \FastD\ServiceProvider\LoggerServiceProvider::class,
         \FastD\ServiceProvider\DatabaseServiceProvider::class,
         \FastD\ServiceProvider\CacheServiceProvider::class,
     ],
@@ -49,20 +56,22 @@ return [
      * Http middleware
      */
     'middleware' => [
-        'basic.auth' => new FastD\BasicAuthenticate\HttpBasicAuthentication([
-            'authenticator' => [
-                'class' => \FastD\BasicAuthenticate\PhpAuthenticator::class,
-                'params' => [
-                    'foo' => 'bar'
-                ]
-            ],
-            'response' => [
-                'class' => \FastD\Http\JsonResponse::class,
-                'data' => [
-                    'msg' => 'not allow access',
-                    'code' => 401
-                ]
+        'basic.auth' => new FastD\BasicAuthenticate\HttpBasicAuthentication(
+            [
+                'authenticator' => [
+                    'class' => \FastD\BasicAuthenticate\PhpAuthenticator::class,
+                    'params' => [
+                        'foo' => 'bar',
+                    ],
+                ],
+                'response' => [
+                    'class' => \FastD\Http\JsonResponse::class,
+                    'data' => [
+                        'msg' => 'not allow access',
+                        'code' => 401,
+                    ],
+                ],
             ]
-        ])
+        ),
     ],
 ];
