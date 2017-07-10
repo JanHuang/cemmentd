26d0ac1c8f3bbfd14ec4b7cc2195d761e600ef24
JanHuang <bboyjanhuang@gmail.com>
2017-07-10 17:17:15 +0800

    update routes

--- admin/config/routes.php
+++ admin/config/routes.php
@@ -7,7 +7,7 @@ route()->group('/api', function () {
     route()->post('/discussions', 'Admin\\Controller\\DiscussionsController@create');
     route()->patch('/discussions/{id}', 'Admin\\Controller\\DiscussionsController@patch');
     route()->delete('/discussions/{id}', 'Admin\\Controller\\DiscussionsController@delete');
-    route()->get('/discussions/{id}/reply', 'Admin\\Controller\\DiscussionsController@reply');
+    route()->get('/discussions/{id}/replies', 'Admin\\Controller\\DiscussionsController@reply');
     route()->get('/users/{id}/discussions', 'Admin\\Controller\\UserDiscussionsController@select');
     route()->get('/target/{id}/discussions', 'Admin\\Controller\\TargetDiscussionsController@select');
 });
